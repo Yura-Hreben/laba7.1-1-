c383ba8471724ff3c27d2e73b42ac89de520abf2
yrii <yural1rn@gmail.com>
2022-11-16 22:48:29 +0200

    laba7.1(1)

---
+++ UnitTest7.1(1)/UnitTest7.1(1).cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab7.1/lab7.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest711
+{
+	TEST_CLASS(UnitTest711)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int s[1] = { 1 };
+			int** r = new int* [5];
+			for (int i = 0; i < 5; i++)
+				r[i] = new int[5];
+			Create(r, 5, 5, 1, 1);
+			int t = r[1][1];
+		}
+	};
+}
